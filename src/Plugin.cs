82bd7a3536262aee866c932a371cce30910dec44
starfish <50672801+starfi5h@users.noreply.github.com>
2024-01-01 13:15:42 +0800

    Add doc

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -45,8 +45,8 @@ namespace LaserClearing
             LocalLaser_Patch.MaxLaserCount = Instance.Config.Bind("Laser", "MaxCount", 3, "Maximum count of laser\n激光最大数量").Value;
             LocalLaser_Patch.Range = Instance.Config.Bind("Laser", "Range", 40f, "Maximum range of laser\n激光最远距离").Value;
             LocalLaser_Patch.MiningTick = Instance.Config.Bind("Laser", "MiningTick", 60, "Time to mine an object (tick)\n开采所需时间").Value;
-            LocalLaser_Patch.CheckIntervalTick = Instance.Config.Bind("Laser", "CheckIntervalTick", 20, "Interval to check object in range\n检查周期").Value;
-            LocalLaser_Patch.MiningPower = Instance.Config.Bind("Laser", "MiningPower", 360f, "Power comsumption per laser (kW)\n激光耗能").Value / 60f * 100f;
+            LocalLaser_Patch.CheckIntervalTick = Instance.Config.Bind("Laser", "CheckIntervalTick", 20, "Interval to check objects in range\n检查周期").Value;
+            LocalLaser_Patch.MiningPower = Instance.Config.Bind("Laser", "MiningPower", 360f, "Power consumption  per laser (kW)\n激光耗能").Value / 60f * 100f;
             Instance.Logger.LogDebug("LoadConfigs");
         }
     }
