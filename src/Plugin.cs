f74c556707b3ea549c7c7edcf8a9493699a5d9b5
starfish <50672801+starfi5h@users.noreply.github.com>
2024-01-01 13:07:03 +0800

    Add config options

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,7 +2,6 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
-using UnityEngine;
 
 [assembly: AssemblyTitle(LaserClearing.Plugin.NAME)]
 [assembly: AssemblyVersion(LaserClearing.Plugin.VERSION)]
@@ -16,13 +15,14 @@ namespace LaserClearing
         public const string NAME = "LaserClearing";
         public const string VERSION = "1.0.0";
 
-        public static ManualLogSource Log;
+        public static Plugin Instance;
         static Harmony harmony;
 
         public void Awake()
         {
-            Log = Logger;
+            Instance = this;
             harmony = new Harmony(GUID);
+            LoadConfigs();
             harmony.PatchAll(typeof(LocalLaser_Patch));
             harmony.PatchAll(typeof(UI_Patch));
 #if DEBUG
@@ -36,6 +36,18 @@ namespace LaserClearing
             UI_Patch.OnDestory();
             harmony.UnpatchSelf();
             harmony = null;
+        }
+
+        public static void LoadConfigs()
+        {
+            LocalLaser_Patch.Enable = Instance.Config.Bind("General", "Enable", true, "Enable LaserClearing").Value;
+            LocalLaser_Patch.EnableLoot = Instance.Config.Bind("General", "EnableLoot", true, "Get drops from destroying trees and stones\n破坏的树木/石头时获取掉落物").Value;
+            LocalLaser_Patch.MaxLaserCount = Instance.Config.Bind("Laser", "MaxCount", 3, "Maximum count of laser\n激光最大数量").Value;
+            LocalLaser_Patch.Range = Instance.Config.Bind("Laser", "Range", 40f, "Maximum range of laser\n激光最远距离").Value;
+            LocalLaser_Patch.MiningTick = Instance.Config.Bind("Laser", "MiningTick", 60, "Time to mine an object (tick)\n开采所需时间").Value;
+            LocalLaser_Patch.CheckIntervalTick = Instance.Config.Bind("Laser", "CheckIntervalTick", 20, "Interval to check object in range\n检查周期").Value;
+            LocalLaser_Patch.MiningPower = Instance.Config.Bind("Laser", "MiningPower", 360f, "Power comsumption per laser (kW)\n激光耗能").Value / 60f * 100f;
+            Instance.Logger.LogDebug("LoadConfigs");
         }
     }
 }
